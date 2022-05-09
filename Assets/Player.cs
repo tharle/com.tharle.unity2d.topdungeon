41fea07fe3d2cf08450fd83f10b5b4978f7c68dd
tharle <tharles.josefidecamargo@gmail.com>
2022-05-09 01:21:45 -0400

    Add some moves

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate() 
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset MoveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction, wether you're going right or left
+        if(moveDelta.x > 0) transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        else if(moveDelta.x < 0) transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+
+        // Make this thing move!
+        transform.Translate(moveDelta * Time.deltaTime);
+        
+        
+    }
+}
