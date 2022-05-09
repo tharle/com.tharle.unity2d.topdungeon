7656a6a527084620c06d8658cecda8245c37b3d1
tharle <tharles.josefidecamargo@gmail.com>
2022-05-09 01:53:11 -0400

    moving and colision

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -24,9 +25,28 @@ public class Player : MonoBehaviour
         // Swap sprite direction, wether you're going right or left
         if(moveDelta.x > 0) transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
         else if(moveDelta.x < 0) transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        
+        // Make sure we can move in this direction, by cating a box there first, if box returns null, we're free to move
+        var distance = Mathf.Abs(moveDelta.x * Time.deltaTime);
+        var direction = new Vector2(moveDelta.x, 0);
+        var layersMask = LayerMask.GetMask("Actor", "Blocking");
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, direction, distance, layersMask);
+        if(hit.collider == null)
+        {
+            // Make this thing move!
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
 
-        // Make this thing move!
-        transform.Translate(moveDelta * Time.deltaTime);
+        // Make sure we can move in this direction, by cating a box there first, if box returns null, we're free to move
+        distance = Mathf.Abs(moveDelta.y * Time.deltaTime);
+        direction = new Vector2(0, moveDelta.y);
+        layersMask = LayerMask.GetMask("Actor", "Blocking");
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, direction, distance, layersMask);
+        if(hit.collider == null)
+        {
+            // Make this thing move!
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
         
         
     }
