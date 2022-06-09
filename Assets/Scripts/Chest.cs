ab4aa5c809b0ae4fed82f130f22f68e66698c1f1
tharle <tharles.josefidecamargo@gmail.com>
2022-06-09 11:57:04 -0400

    Interact with chest and collect some pesos

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectable
+{
+    public Sprite emptyChestSprite;
+    public int pesosAmount = 5;
+    protected override void OnCollect()
+    {
+        if (!collected)
+        {
+            collected = true;
+            GetComponent<SpriteRenderer>().sprite = emptyChestSprite;
+            string txtPesosAmount = pesosAmount > 1 ? "pesos" : "peso";
+            Debug.Log($"Grant {pesosAmount} {txtPesosAmount}!");
+        }
+    }
+
+}
